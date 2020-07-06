f9fa87dcae987ad81f2ea268251374d866874c92
Lucas <lucaswfreeman@gmail.com>
2020-07-06 17:40:14 -0500

    Adding in Steam Sale Functionality

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import json
 import scraper
+import steam_sale
 import discord
 import os
 from dotenv import load_dotenv
@@ -14,7 +15,33 @@ load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
 
 
-async def update_function(ctx):
+async def update_sales(ctx):
+    message_text = ""
+    ctx.channel = bot.get_channel(729799766153887744)
+    async for message in ctx.channel.history(limit=200):
+        if not message.pinned:
+            await message.delete()
+        elif message.id == 729809509979127818:
+            sales_board_message = message
+    async with ctx.channel.typing():
+        # await ctx.send("[Steam Sales Go Here]")
+        await sales_board_message.edit(content="Fetching data...one moment")
+        sale_data = await steam_sale.fetch_sale_data()
+        i = 1
+        for key in sale_data.keys():
+            if i < 16:
+                message_text = f'{message_text}{key} >> \
+{sale_data[key]["Price"]}\
+({sale_data[key]["Discount"]} off): \
+{sale_data[key]["Steam URL"]}\n\n'
+            else:
+                break
+            i += 1
+        message_text = f"{message_text}:star:Last Updated: *{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} Central*:star:"
+        await sales_board_message.edit(content=message_text)
+
+
+async def update_leaderboard(ctx):
     url_list = []
     ctx.channel = bot.get_channel(729482860292997180)
     message_text = ""
@@ -36,14 +63,21 @@ async def update_function(ctx):
         await leaderboard_message.edit(content=message_text)
 
 
-async def hourly_update():
-    await bot.wait_until_ready()
-    channel = bot.get_channel(729094984069939250)
+async def fetch_context_from_channel_with_pin(channel_id):
+    channel = bot.get_channel(channel_id)
     async for message in channel.history(limit=200):
         if message.pinned:
             ctx = await bot.get_context(message)
+            return ctx
+
+
+async def hourly_update():
+    await bot.wait_until_ready()
     while True:
-        await update_function(ctx)
+        ctx = await fetch_context_from_channel_with_pin(729094984069939250)
+        await update_leaderboard(ctx)
+        ctx = await fetch_context_from_channel_with_pin(729799766153887744)
+        await update_sales(ctx)
         await asyncio.sleep(3600)
 
 
@@ -72,7 +106,14 @@ async def on_ready():
 
 @bot.command()
 async def update(ctx):
-    await update_function(ctx)
+    await update_leaderboard(ctx)
+
+
+@bot.command()
+async def sales(ctx):
+    await update_sales(ctx)
+
+# 729799766153887744
 
 bot.loop.create_task(hourly_update())
 bot.run(discord_token)
